6d350034eb291ab21083ac5eebbfa4b1ea2df712
Ivan Pettinga <Ivanpettinga@outlook.com>
2023-04-12 16:34:28 +0200

    So much refactoring, wow

--- src/generate_csv.py
+++ src/generate_csv.py
@@ -1,20 +1,31 @@
 import os
-from scripts.delisted_participants import scrape_delisted_participants
-from scripts.listed_participants import scrape_listed_participants
+import pandas as pd
+from scripts.functions import check_url_status 
+from scripts.functions import get_entries
 
+active= 'https://unglobalcompact.org/what-is-gc/participants/search?page=1&search%5Bkeywords%5D=&search%5Bper_page%5D=50&search%5Breporting_status%5D%5B%5D=active&search%5Bsort_direction%5D=asc&search%5Bsort_field%5D=&utf8=%E2%9C%93'
+non_communicating = 'https://unglobalcompact.org/what-is-gc/participants/search?page=1&search%5Bkeywords%5D=&search%5Bper_page%5D=50&search%5Breporting_status%5D%5B%5D=noncommunicating&search%5Bsort_direction%5D=asc&search%5Bsort_field%5D=&utf8=%E2%9C%93'
+delisted = 'https://unglobalcompact.org/what-is-gc/participants/search?page=1&search%5Bkeywords%5D=&search%5Breporting_status%5D%5B%5D=delisted&search%5Bsort_field%5D=&search%5Bsort_direction%5D=asc&search%5Bper_page%5D=50'
 
-url_active= 'https://unglobalcompact.org/what-is-gc/participants/search?utf8=%E2%9C%93&search%5Bkeywords%5D=&search%5Breporting_status%5D%5B%5D=active&search%5Bsort_field%5D=&search%5Bsort_direction%5D=asc&search%5Bper_page%5D=50'
-url_non_communicating = 'https://unglobalcompact.org/what-is-gc/participants/search?utf8=%E2%9C%93&search%5Bkeywords%5D=&search%5Breporting_status%5D%5B%5D=noncommunicating&search%5Bsort_field%5D=&search%5Bsort_direction%5D=asc&search%5Bper_page%5D=50'
-url_delisted= 'https://unglobalcompact.org/what-is-gc/participants/search?utf8=%E2%9C%93&search%5Bkeywords%5D=&search%5Breporting_status%5D%5B%5D=delisted&search%5Bsort_field%5D=&search%5Bsort_direction%5D=asc&search%5Bper_page%5D=50'
-
+print('Welcome to the webscraping model of the Global Compact List')
 result_location = result_location = input("Enter the results path: ")
-
 print("Loading data from webpage, please wait")
 
-listed_participants = scrape_listed_participants()
-delisted_participants = scrape_delisted_participants()
+global_compact_list = pd.DataFrame(columns= ['Participant','Country','Year','Sector'])
 
-listed_participants.to_csv(os.path.join(result_location, "Global_compact_listed.csv"))
-delisted_participants.to_csv(os.path.join(result_location, "Global_compact_delisted.csv"))
+urls = [active, non_communicating, delisted]
+
+status_map = {urls[0]: 'active', urls[1]: 'non_communicating', urls[2]: 'delisted'}
+
+for statusframe in urls:
+    if check_url_status(statusframe) == 'status: OK':
+        entries = get_entries(statusframe)
+        entries['Status'] = status_map[statusframe]
+        global_compact_list = pd.concat([global_compact_list, entries])
+    else:
+        print("The website URL is not accessible")
+
+global_compact_list = global_compact_list.replace(',', '.', regex=True)
+global_compact_list.to_csv(os.path.join(result_location, "Global Compact List.csv"), index=False)
 
 print("Finished")
--- src/scripts/functions.py
+++ src/scripts/functions.py
@@ -5,7 +5,6 @@ import urllib3
 from http.client import responses
 import time as time
 
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def check_url_status (url):
@@ -27,23 +26,27 @@ def get_parameters (url):
     for page in max_pages:
         page_number = page.get_text()
 
-    number_of_options_searches = ''.join([n for n in max_searches if n.isdigit()][-2:])
+    number_of_options_searches = int(''.join([n for n in max_searches if n.isdigit()][-2:]))
     number_of_entries = int(''.join([n for n in page_number if n.isdigit()]))
     number_of_requests = (round(number_of_entries/number_of_options_searches))
 
     return number_of_requests
 
 def get_entries (url):
-    requests = get_parameters(url)
+    requests = get_parameters(url) + 1
     
     company_list = []
     sector_list = []
     country_list = []
     year_list = []
 
-    for page_number in range(1,int(requests)):
+    for page_number in range(1,int(3)): #requests
         time.sleep(1)
-        webpage_response = rs.get()
+        page_string = 'search?page=1'
+        replacement = 'search?page=' + str(page_number)
+        url_for_current_page = url.replace(page_string, replacement)
+        print(str(page_number) + ' of the ' + str(requests) + ' requests completed')
+        webpage_response = rs.get(url_for_current_page)
         webpage = webpage_response.content
         soup = bs(webpage, 'html.parser')
 
@@ -52,21 +55,27 @@ def get_entries (url):
         country = soup.find_all(attrs={'class':'country'})
         year = soup.find_all(attrs={'class':'joined-on'})
     
-    for individual_name in names:
-        company = individual_name.get_text(strip = True)
-        company_list.append(company)
+        for individual_name in names:
+            company = individual_name.get_text(strip = True)
+            company_list.append(company)
 
-    for individual_sector in sector:
-        sector_company = individual_sector.get_text(strip = True)
-        sector_list.append(sector_company)
+        for individual_sector in sector:
+            sector_company = individual_sector.get_text(strip = True)
+            sector_list.append(sector_company)
 
-    for individual_country in country:
+        for individual_country in country:
             country_company = individual_country.get_text(strip = True)
             country_list.append(country_company)
         
-    for individual_year in year:
-        year_company = individual_year.get_text(strip = True)
-        year_list.append(year_company)
-    
+        for individual_year in year:
+            year_company = individual_year.get_text(strip = True)
+            year_list.append(year_company)
 
+    dataframe_name = pd.DataFrame(company_list[1:], columns = ['Participant'])
+    dataframe_country = pd.DataFrame(country_list[1:], columns = ['Country'])
+    dataframe_year = pd.DataFrame(year_list[1:], columns = ['Year'])
+    dataframe_sector = pd.DataFrame(sector_list[1:], columns = ['Sector'])
 
+    listed_concatenated_dataframes = pd.concat([dataframe_name, dataframe_sector,dataframe_country,dataframe_year], axis=1)
+
+    return listed_concatenated_dataframes
